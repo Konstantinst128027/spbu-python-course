f9fce9c69bc87edc3963e32f72cc0f09efd35472
Konstantinst128027 <st128027@student.spbu.ru>
2025-10-27 23:03:23 +0300

    I corrected my mistakes

--- project/Task4/roulet.py
+++ project/Task4/roulet.py
@@ -37,6 +37,7 @@ class Roulet:
         """
         Initializes the roulette wheel
         """
+        self.numbers: typing.List[int] = list(range(37))
         self.red_numbers: typing.List[int] = [
             1,
             3,
